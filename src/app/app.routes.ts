9dfd21b9cef27ce6551e6fb7619c4b7f01d7dae5
abhishek.kale <abhisahebpatil@gmail.com>
2026-02-11 20:56:31 +0530

    update files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-    {path:'',redirectTo:'',pathMatch:'full'},
-    {path:'',loadComponent:()=> import('./component/valuechanges-reactive-form/valuechanges-reactive-form').then((c)=>c.ValuechangesReactiveForm)}
-];
+export const routes: Routes = [];
